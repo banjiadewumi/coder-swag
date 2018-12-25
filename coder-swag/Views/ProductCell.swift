62776590f763ababdf9a9bd2f8abfd9aa465fd22
Banji Adewumi <banji@Banjis-Mac.local>
2018-12-24 21:00:48 -0800

    add products page

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Banji Adewumi on 12/24/18.
+//  Copyright © 2018 Banji Adewumi. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
